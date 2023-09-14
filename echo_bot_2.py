f6df3472d19eb615189649f1b44323b93a03b5fa
“Muzyria” <fila090580@gmail.com>
2023-09-14 15:53:53 +0300

    new

--- echo_bot_2.py
+++ echo_bot_2.py
@@ -32,17 +32,31 @@ async def send_photo_echo(message: Message):
     await message.reply_photo(message.photo[0].file_id)
 
 
-# Этот хэндлер будет срабатывать на отправку боту голосовым сообщением
-async def send_voice_echo(message: Message):
+# # Этот хэндлер будет срабатывать на отправку боту голосовым сообщением
+# async def send_voice_echo(message: Message):
+#     print(message)
+#     await message.answer_voice(message.voice.file_id)
+# Навешиваем декоратор с указанием в качестве фильтра типа контента
+@dp.message(F.voice)
+async def process_sent_voice(message: Message):
+    # Выводим апдейт в терминал
     print(message)
-    await message.answer_voice(message.voice.file_id)
+    # Отправляем сообщение в чат, откуда пришло голосовое
+    await message.answer(text='Вы прислали голосовое сообщение!')
 
 
 # Этот хэндлер будет срабатывать на отправку боту стикер
 async def send_sticker_echo(message: Message):
-    print(message)
+    print(message.model_dump_json(indent=4, exclude_none=True))
     await message.answer_sticker(message.sticker.file_id)
 
+
+@dp.message(F.video_note)
+async def process_sent_v(message: Message):
+    # Выводим апдейт в терминал
+    print(message)
+    # Отправляем сообщение в чат, откуда пришло
+    await message.answer(text='Вы прислали video_note сообщение!')
 
 # Этот хэндлер будет срабатывать на отправку боту видео
 async def send_video_echo(message: Message):
@@ -60,7 +74,7 @@ async def send_echo(message: Message):
 dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
 dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
-dp.message.register(send_voice_echo, F.content_type == ContentType.VOICE)
+# dp.message.register(send_voice_echo, F.content_type == ContentType.VOICE)
 dp.message.register(send_sticker_echo, F.content_type == ContentType.STICKER)
 dp.message.register(send_video_echo, F.content_type == ContentType.VIDEO)
 
