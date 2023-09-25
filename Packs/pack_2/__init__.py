c676061612e97e17082f85be1e169f442a5c9673
“Muzyria” <fila090580@gmail.com>
2023-09-25 15:10:21 +0300

    new

--- Packs/pack_2/__init__.py
+++ Packs/pack_2/__init__.py
@@ -2,5 +2,4 @@
 print('Это файл __init__.py пакета pack_2, его имя во время выполнения программы:', __name__)
 
 
-# import pack_2.file_21
-from pack_2.file_21 import another_some_func
+from .pack_21 import *
