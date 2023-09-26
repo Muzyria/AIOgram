9cebf4cd40c11b9958d1723338f6ae5f33a7638c
“Muzyria” <fila090580@gmail.com>
2023-09-26 15:14:18 +0300

    new

---
+++ config_bot_lesson/bot.py
@@ -0,0 +1,13 @@
+
+from config_bot_lesson.config import load_config
+
+
+config = load_config('.env')
+
+
+
+bot_token = config.tg_bot.token           # Сохраняем токен в переменную bot_token
+superadmin = config.tg_bot.admin_ids[0]   # Сохраняем ID админа в переменную superadmin
+
+print(bot_token)
+print(superadmin)
